37faf1572cc6be971ca71d0a63964f7bc903a9b9
BichoyEmad <bichoy.emad@gmail.com>
2025-12-23 21:02:31 +0200

    fix: snow effect

--- src/components/SnowEffect.jsx
+++ src/components/SnowEffect.jsx
@@ -23,7 +23,7 @@ const SnowEffect = () => {
             width: '100%',
             height: '100%',
             pointerEvents: 'none',
-            zIndex: 0
+            zIndex: 100
         }}>
             {snowflakes.map((flake) => (
                 <div
